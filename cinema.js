b26f373e2f8b38f0c4a6ad521de731998ad28711
Sergio Ribeiro <grisly01@gmail.com>
2022-10-23 15:13:58 -0300

    primeira versão do projeto cinehouse

---
+++ cinema.js
@@ -0,0 +1,5 @@
+//declaramos variavel cinema e atribuimos a escrita, string cinehouse
+//let cinema = "CineHouse"
+//console.log(cinema)
+const catalogo = require('./database/catalogo.json')
+console.log(catalogo)
